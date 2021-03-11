6f6ae91a1b9ad745929c4689db694e790a430ddd
GMihalkow <georgimihalkov@abv.bg>
2021-03-11 23:07:01 +0200

    feat: initial commit, implemented basic player ship movement

---
+++ Assets/Scripts/Control/Mover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SpaceFighter.Control
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] float _speed = 10f;
+
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            this._rigidbody = this.GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            var xAxis = Input.GetAxis("Horizontal");
+            var yAxis = Input.GetAxis("Vertical");
+
+            this._rigidbody.AddForce(new Vector2(xAxis * this._speed, yAxis * this._speed), ForceMode2D.Impulse);
+        }
+    }
+}

b0f3b969114c8fa4ed0cd9517570ed2f307cff67
GMihalkow <georgimihalkov@abv.bg>
2021-03-15 21:27:55 +0200

    fix: reworked the player movement, removed unneeded colliders

--- Assets/Scripts/Control/Mover.cs
+++ Assets/Scripts/Control/Mover.cs
@@ -6,19 +6,36 @@ namespace SpaceFighter.Control
     {
         [SerializeField] float _speed = 10f;
 
-        private Rigidbody2D _rigidbody;
+        private Camera _mainCamera;
+        private SpriteRenderer _spriteRenderer;
+        private float _minY;
+        private float _maxY;
+        private float _minX;
+        private float _maxX;
 
         private void Awake()
         {
-            this._rigidbody = this.GetComponent<Rigidbody2D>();
+            this._spriteRenderer = this.GetComponent<SpriteRenderer>();
+            this._mainCamera = Camera.main;
+
+            this._maxY = this._mainCamera.orthographicSize;
+            this._minY = this._maxY * -1;
+            this._maxX = this._mainCamera.orthographicSize * this._mainCamera.aspect;
+            this._minX = this._maxX * -1;
         }
 
         private void Update()
         {
+            // TODO [GM]: Extract input handling to a PlayerController?
             var xAxis = Input.GetAxis("Horizontal");
             var yAxis = Input.GetAxis("Vertical");
 
-            this._rigidbody.AddForce(new Vector2(xAxis * this._speed, yAxis * this._speed), ForceMode2D.Impulse);
+            var currPos = this.transform.position;
+            var x = Mathf.Clamp((xAxis * this._speed * Time.deltaTime) + currPos.x, this._minX, this._maxX);
+            var y = Mathf.Clamp((yAxis * this._speed * Time.deltaTime) + currPos.y, this._minY, this._maxY);
+            var futurePosition = new Vector3(x, y);
+
+            this.transform.position = futurePosition;
         }
     }
 }

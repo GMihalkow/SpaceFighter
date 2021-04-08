4027f599beb3155f6fa0fe4f3834337905d31fdf
GMihalkow <georgimihalkov@abv.bg>
2021-04-09 01:27:45 +0300

    fix: reintroduced the ParticleDestroyer class, because of Unity 2019's bug with the ParticleSystem Stop Action which ignores the sub emitters

---
+++ Assets/Scripts/Core/ParticleDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SpaceFighter.Core
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class ParticleDestroyer : MonoBehaviour
+    {
+        private ParticleSystem _ps;
+
+        private void Awake()
+        {
+            this._ps = this.GetComponent<ParticleSystem>();
+        }
+
+        private void Update()
+        {
+            if (this._ps.isStopped) GameObject.Destroy(this.gameObject);
+        }
+    }
+}

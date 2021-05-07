32c66877799a7868b72109ac2a3a1c89a6c9a6a1
GMihalkow <georgimihalkov@abv.bg>
2021-05-07 15:12:22 +0300

    feat: implemented menu play button

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SpaceFighter.Menu
+{
+    public class Menu : MonoBehaviour
+    {
+        public void OnPlay()
+        {
+            SceneManager.LoadSceneAsync("PlayScene", LoadSceneMode.Single);
+        }
+    }
+}

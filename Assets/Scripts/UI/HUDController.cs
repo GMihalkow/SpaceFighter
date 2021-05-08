b7ba024125db67886a314b89fb5b0d9fe940a60e
GMihalkow <georgimihalkov@abv.bg>
2021-05-08 23:08:55 +0300

    feat: implemented the Game Over screen and added a button to restart the game

---
+++ Assets/Scripts/UI/HUDController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace SpaceFighter.UI
+{
+    public class HUDController : MonoBehaviour
+    {
+        [SerializeField] GameObject _gameOverScreen;
+
+        /// <summary>
+        /// Called when player dies
+        /// </summary>
+        public void OnPlayerDeath()
+        {
+            if (this._gameOverScreen == null) throw new InvalidOperationException("No game end screen prefab found!");
+
+            var gameOverScreen = GameObject.Instantiate(this._gameOverScreen);
+            gameOverScreen.GetComponentInChildren<Canvas>().worldCamera = Camera.main;
+
+            var btn = gameOverScreen.GetComponentInChildren<Button>();
+            btn?.onClick.AddListener(this.RestartGame);
+        }
+
+        private void RestartGame()
+        {
+            SceneManager.LoadSceneAsync("PlayScene", LoadSceneMode.Single);
+        }
+    }
+}

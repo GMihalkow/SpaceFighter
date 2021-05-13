70970631423f7eefb4096abc75fd6373b57d9770
GMihalkow <georgimihalkov@abv.bg>
2021-05-13 20:47:36 +0300

    fix: added min and max spawn time frequencies to the menu items spawner

--- Assets/Scripts/Spawn/MenuItemsSpawner.cs
+++ Assets/Scripts/Spawn/MenuItemsSpawner.cs
@@ -7,28 +7,32 @@ namespace SpaceFighter.Spawn
     {
         [SerializeField] GameObject _containerPrefab;
         [SerializeField] string _spritesDirectory = "Planets/";
-        [SerializeField] float _spawnFrequency = 15f;
+        [SerializeField] float _maxSpawnFrequency = 15f;
+        [SerializeField] float _minSpawnFrequency = 5f;
         [SerializeField] float _spawnOffset = 3f;
 
         private float _spawnTimeout;
         private MapBounds _mapBounds;
         private Sprite[] _sprites;
+        private float _currentSpawnFrequency;
 
         private void Awake()
         {
+            this._currentSpawnFrequency = Random.Range(this._minSpawnFrequency, this._maxSpawnFrequency);
             this._mapBounds = Camera.main.GetComponent<MapBounds>();
             this._sprites = Resources.LoadAll<Sprite>(this._spritesDirectory);
         }
 
         private void Update()
         {
-            if (Mathf.Approximately(this._spawnTimeout, this._spawnFrequency) || this._spawnTimeout >= this._spawnFrequency)
+            if (Mathf.Approximately(this._spawnTimeout, this._currentSpawnFrequency) || this._spawnTimeout >= this._currentSpawnFrequency)
             {
                 var pos = this._mapBounds.GeneratePositionLeftOfCamera(this._spawnOffset);
                 
                 this.Spawn(pos);
 
                 this._spawnTimeout = 0f;
+                this._currentSpawnFrequency = Random.Range(this._minSpawnFrequency, this._maxSpawnFrequency);
             }
 
             this._spawnTimeout += Time.deltaTime;

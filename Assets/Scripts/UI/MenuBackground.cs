23c309812424173475522c4495ec195677b07b99
GMihalkow <georgimihalkov@abv.bg>
2021-05-16 11:26:43 +0300

    feat: implemented looping menu background

---
+++ Assets/Scripts/UI/MenuBackground.cs
@@ -0,0 +1,53 @@
+﻿using SpaceFighter.Core;
+using SpaceFighter.Movement;
+using System;
+using UnityEngine;
+
+namespace SpaceFighter.UI
+{
+    public class MenuBackground : MonoBehaviour
+    {
+        private MapBounds _mapBounds;
+        private Mover _mover;
+        private SpriteRenderer _leftTile;
+        private bool _hasPassed;
+
+        public event EventHandler<Vector3> OnLeave;
+
+        public bool HasPassed => this._hasPassed;
+
+        private void Awake()
+        {
+            this._mapBounds = Camera.main.GetComponent<MapBounds>();
+            this._leftTile = this.transform.Find("LeftBg").GetComponent<SpriteRenderer>();
+            this._mover = this.GetComponent<Mover>();
+        }
+
+        private void Update()
+        {
+            var edgePos = this._leftTile.transform.position;
+            edgePos.x -= (this._leftTile.bounds.size.x / 2);
+
+            if (edgePos.x >= this._mapBounds.MaxX)
+            {
+                GameObject.Destroy(this.gameObject);
+                return;
+            }
+
+            this._mover.Move(Vector3.right);
+        }
+
+        private void LateUpdate()
+        {
+            if (this._leftTile.transform.position.x < this._mapBounds.MinX || this._hasPassed) return;
+
+            var edgePos = this._leftTile.transform.position;
+            edgePos.x -= (this._leftTile.bounds.size.x / 2);
+            edgePos.x -= this._leftTile.bounds.size.x - (this._leftTile.bounds.size.x / 2);
+
+            this._hasPassed = true;
+
+            this.OnLeave.Invoke(this, edgePos);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MenuBackgroundController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SpaceFighter.UI
+{
+    public class MenuBackgroundController : MonoBehaviour
+    {
+        [SerializeField] GameObject _bgMoverPrefab;
+
+        private void Start()
+        {
+            for (int index = 0; index < this.transform.childCount; index++)
+            {
+                var bg = this.transform.GetChild(index);
+                bg.GetComponent<MenuBackground>().OnLeave += this.OnLeave;
+            }
+        }
+        
+        private void OnLeave(object sender, Vector3 pos)
+        {
+            var instance = GameObject.Instantiate(this._bgMoverPrefab, pos, Quaternion.identity, this.transform);
+            instance.GetComponent<MenuBackground>().OnLeave += this.OnLeave;
+        }
+    }
+}

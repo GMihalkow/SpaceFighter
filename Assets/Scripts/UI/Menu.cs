52ea58774a74997ff163ad429f44bec2e222cb08
GMihalkow <georgimihalkov@abv.bg>
2021-05-17 21:42:35 +0300

    feat: implemented menu button sound and background sounds

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,15 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace SpaceFighter.UI
 {
     public class Menu : MonoBehaviour
     {
+        [SerializeField] float _playBtnDelay = 0.05f;
+
+        private bool _btnIsClicked;
+
         /// <summary>
         /// Called from canvas button
         /// </summary>
         public void OnPlay()
         {
+            if (this._btnIsClicked) return;
+
+            this._btnIsClicked = true;
+
+            this.StartCoroutine(this.OnPlayCoroutine());
+        }
+
+        IEnumerator OnPlayCoroutine()
+        {
+            yield return new WaitForSecondsRealtime(this._playBtnDelay);
+
             SceneManager.LoadSceneAsync("PlayScene", LoadSceneMode.Single);
         }
     }

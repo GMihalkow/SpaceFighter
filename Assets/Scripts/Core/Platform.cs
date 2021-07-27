732a41f1f0b390affe20ef48d699dd6855d0a0a4
GMihalkow <georgimihalkov@abv.bg>
2021-07-27 21:22:56 +0300

    feat: implemented mobile webgl recognition

---
+++ Assets/Scripts/Core/Platform.cs
@@ -0,0 +1,19 @@
+﻿namespace SpaceFighter.Core
+{
+    public static class Platform
+    {
+        public static bool IsMobileBrowser
+        {
+            get
+            {
+#if UNITY_EDITOR
+                return false;
+#elif UNITY_WEBGL
+    return WebGLHandler.IsMobileBrowser();
+#else
+    return false;
+#endif
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/WebGLHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace SpaceFighter.Core
+{
+    public class WebGLHandler : MonoBehaviour
+    {
+        [DllImport("__Internal")]
+        public static extern bool IsMobileBrowser();
+    }
+}

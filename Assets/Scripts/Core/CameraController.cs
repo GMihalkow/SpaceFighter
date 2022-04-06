136a2ef7c958d828ccb16bca90a09e2a53712b5f
GMihalkow <georgimihalkov@abv.bg>
2022-04-06 21:14:42 +0300

    feat: added logic that sets the screen resolution according to the device

---
+++ Assets/Scripts/Core/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SpaceFighter.Core
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] bool _recalculateResolution;
+
+        private void Awake()
+        {
+            if (!this._recalculateResolution) return;
+
+            Screen.SetResolution(Screen.width, Screen.height, true);
+        }
+    }
+}
